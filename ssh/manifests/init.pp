68e42e84ae25c437069d2bd71894a9f6d8f7e54f
root <root@ahsanjaved1.mylabserver.com>
2018-06-04 13:18:56 +0000

    adding ssh mocule

---
+++ ssh/manifests/init.pp
@@ -0,0 +1,8 @@
+class ssh(
+  $package_name = $::ssh::params::package_name,
+  $servive_name = $::ssh::params::service_name,
+  )inherits ::ssh::params {
+  class {'ssh::install': } ->
+  class {'ssh::service': }
+
+}
